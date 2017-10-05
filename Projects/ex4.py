4986c2a5912200c691297275618fc15d0d8effe9
[AKegge] <[s186229@uos.ac.uk]>
2017-10-05 10:13:39 +0100

    [changed sx4 to print si units first]

--- Projects/ex4.py
+++ Projects/ex4.py
@@ -12,8 +12,8 @@ my_height_ft = int(my_height / 12) #convert my_height to feet
 my_height_in = my_height % 12 #and inches
 
 print(f"Let's talk about {my_name}.")
-print(f"He is {my_height_ft} feet and {my_height_in} inches tall ({my_height} inches total), or {my_height_m} metres tall ({my_height_cm} centimetres).")
-print(f"He weighs {my_weight} pounds, or {my_weight_kg} kilograms.")
+print(f"He is {my_height_m} metres tall ({my_height_cm} centimetres), or {my_height_ft} feet and {my_height_in} inches tall ({my_height} inches total).")
+print(f"He weighs {my_weight_kg} kilograms, or {my_weight} pounds.")
 print(f"It is {is_heavy} that he is overweight.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 
