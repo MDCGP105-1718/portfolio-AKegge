90d6dee56cd7202e3f9c62c23fcb4080f0ac4e71
[AKegge] <[s186229@uos.ac.uk]>
2017-10-09 13:02:40 +0100

    [compleated ex5]

---
+++ Projects/ex5(for).py
@@ -0,0 +1,5 @@
+for bottles in range(99, 1, -1): #I prefer while when using python, with for you have to remember (start, stop, step) and that it does while start!stop:  ...  start+=step
+    print (f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
+    print (f"Take one down, pass it around, {bottles} bottles of beer on the wall…")
+print ("1 bottle of beer on the wall, 1 bottle of beer!")
+print ("So take it down, pass it around, no more bottles of beer on the wall!")
---
+++ Projects/ex5(while).py
@@ -0,0 +1,7 @@
+bottles = 99
+while bottles > 1: #I prefer while when using python because it is more obvious what it is doing
+    print (f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
+    bottles-=1
+    print (f"Take one down, pass it around, {bottles} bottles of beer on the wall…")
+print ("1 bottle of beer on the wall, 1 bottle of beer!")
+print ("So take it down, pass it around, no more bottles of beer on the wall!")
